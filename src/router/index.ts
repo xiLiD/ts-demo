c41059e358a9eed70214860299c3032568a9b7a6
RT-king7 <39544196+RT-king7@users.noreply.github.com>
2020-11-11 12:23:15 +0800

    ts-提交

---
+++ src/router/index.ts
@@ -0,0 +1,28 @@
+import Vue from 'vue';
+import VueRouter, { RouteConfig } from 'vue-router';
+import Home from '../views/Home.vue';
+
+// Vue.use(VueRouter);
+if (!window.VueRouter) {
+  Vue.use(VueRouter);
+}
+
+const routes: RouteConfig[] = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+];
+
+const router = new VueRouter({
+  routes,
+});
+
+export default router;
---
+++ src/views/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <iframe
+      :key="item.tag"
+      v-once
+      :src="www.baidu.com"
+      frameborder="0"
+    ></iframe>
+  </div>
+</template> 
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+@Component({
+  components: {
+    About,
+  },
+})
+export default class About extends Vue {
+  public message: string = '消息';
+  public queryInfo: any = {};
+  private created() {
+    console.log(this.message);
+  }
+}
+</script>
